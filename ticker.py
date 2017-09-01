0f310be37c2e10da1ce8a9ff75c31627481814d1
sm0ke <sm0ke@greenb0x.local>
2017-09-01 14:48:33 -0600

    added LED indicators on price change

--- ticker.py
+++ ticker.py
@@ -16,6 +16,7 @@ lcd.set_cursor_position(0,0); lcd.write("Bittrex BTC-PAY:")
 #URL = 'https://www.bitstamp.net/api/ticker/'
 URL = "https://bittrex.com/api/v1.1/public/getmarketsummary?market=btc-pay"
 string = ""
+last = 0
 
 class Ticker(MenuOption):
     def redraw(self,menu):
@@ -35,15 +36,29 @@ menu = Menu(
 
 menu.right()
 
-"updates the price string from bitstamp every 5 seconds"
+"""
+updates the price string from bitstamp every 5 seconds
+sets the HAT LED graph when price changes.
+LED 0 is on if up.  LED 5 is on if down
+"""
+
 def update():
     while True:
-        global string
-        r = requests.get(URL)
-        #priceFloat = float(json.loads(r.text)['last'])
-	price = json.loads(r.text)['result'][0]['Last']
-        string = "$" + str(price) + "/PAY"
-        sleep(5)
+        global string, last
+        r = requests.get(URL, verify=False)
+        #uncomment to use bitstamp api
+        #priceFloat = float(json.loads(r.text)['Last'])
+        price = float(json.loads(r.text)['result'][0]['Last'])
+        if price > last:
+            backlight.graph_set_led_state(0,1)
+            backlight.graph_set_led_state(5,0)
+            last = price
+        elif price < last:
+            backlight.graph_set_led_state(0,0)
+            backlight.graph_set_led_state(5,1)
+            last = price
+        string = "$" + str(price)
+        sleep(0.5)
 
 "redraws the LCD screen with latest prices"
 def scroller():
